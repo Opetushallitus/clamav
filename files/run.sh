6a2db8a59ac3bc5f9de11fa7b71e453c66695690
Esa Virtanen <esa.virtanen@reaktor.com>
2018-10-31 14:39:14 +0200

    Set max file size to match max liiteri file size

--- files/run.sh
+++ files/run.sh
@@ -2,4 +2,4 @@ freshclam
 clamd &
 freshclam -d
 
-java -Xms768m -Xmx768m -jar /root/clamav-rest.jar --clamd.port=3310 --clamd.host=localhost --clamd.timeout=5000
+java -Xms768m -Xmx768m -jar /root/clamav-rest.jar --clamd.port=3310 --clamd.host=localhost --clamd.timeout=5000 --clamd.maxfilesize=1024MB --clamd.maxrequestsize=1024MB
