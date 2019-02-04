4f18c33aa260a39e019d6c9c21a6c66ca747bdbc
Jani Rahkola <jani.rahkola@reaktor.com>
2019-02-04 14:06:54 +0200

    3gb heap

--- files/run.sh
+++ files/run.sh
@@ -3,4 +3,4 @@ set -eu
 freshclam
 clamd &
 freshclam -d
-java -Xms3584m -Xmx3584m -jar /root/clamav-rest.jar --clamd.port=3310 --clamd.host=localhost --clamd.timeout=480000 --clamd.maxfilesize=1024MB --clamd.maxrequestsize=1024MB
+java -Xms3g -Xmx3g -jar /root/clamav-rest.jar --clamd.port=3310 --clamd.host=localhost --clamd.timeout=480000 --clamd.maxfilesize=1024MB --clamd.maxrequestsize=1024MB
