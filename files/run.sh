22e291210a36e0bbb419c2e2c42dac9f48d52364
Harri Salokorpi <harri.salokorpi@reaktor.com>
2020-01-08 15:35:47 +0200

    HLE-1134: Mikäli clamav kaatuu, sammuta kontti

--- files/run.sh
+++ files/run.sh
@@ -5,6 +5,6 @@ for dir in run varlog varlib; do
 done
 
 freshclam
-clamd &
 freshclam -d
-java -Xms3g -Xmx3g -jar /usr/local/bin/clamav-rest.jar --clamd.port=3310 --clamd.host=localhost --clamd.timeout=1200000 --clamd.maxfilesize=1024MB --clamd.maxrequestsize=1024MB --clamd.qos.maxrequests=1 --clamd.qos.waitms=100
+java -Xms3g -Xmx3g -jar /usr/local/bin/clamav-rest.jar --clamd.port=3310 --clamd.host=localhost --clamd.timeout=1200000 --clamd.maxfilesize=1024MB --clamd.maxrequestsize=1024MB --clamd.qos.maxrequests=1 --clamd.qos.waitms=100 &
+clamd -F
